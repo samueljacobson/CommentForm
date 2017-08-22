869e9ab35c01f1bd881aebbdf2fe5357f358a11d
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-08-22 11:01:13 -0400

    first commit

---
+++ CommentForm/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+
+        [ForeignKey("Procedure")]
+        public int ProcedureID { get; set; }
+        public virtual Procedure Procedure { get; set; }
+    }
+}
---
+++ CommentForm/Models/Procedure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm.Models
+{
+    public class Procedure
+    {
+        [Key]
+        public int ProcedureID { get; set; }
+
+        public int Priority { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
